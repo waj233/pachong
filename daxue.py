798bba7e754a38761751af8a71dcd61acb369d97
wangaijun <1278490449@qq.com>
2020-04-10 14:50:55 +0800

    提交信息

---
+++ daxue.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+
+# 从网络上获取大学排名网页的内容
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+
+# 提取网页内容中信息到合适的数据结构（列表）
+def fillUnivList(ulist, html):
+    soup = BeautifulSoup(html, "html.parser")
+    for tr in soup.find('tbody').children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr('td')
+            ulist.append([tds[0].string, tds[1].string, tds[3].string])
+
+
+# 利用数据结构（列表）展示并输出结果
+def printUnivList(ulist, num):
+    tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
+    print(tplt.format("排名", "学校名称", "总分", chr(12288)))
+    for i in range(num):
+        u = ulist[i]
+        print(tplt.format(u[0], u[1], u[2], chr(12288)))
+    print("Suc" + str(num))
+
+
+def main():
+    uinfo = []
+    url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2019.html'
+    html = getHTMLText(url)
+    fillUnivList(uinfo, html)
+    printUnivList(uinfo, 20)  # 打印前20所大学信息
+
+
+main()
